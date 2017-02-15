05f74173be5b769789a6c4a00bbf9d035718a733
Name Name, Jr <fluxpatron@gmail.com>
2017-02-15 12:06:22 -0600

    Add files via upload

---
+++ thumbBox_script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+		//specify selectors by using $(), use # to specify an id
+	$("#nextButton") .click(function(){
+		$("#thumbBox").css("background-image","/pictures.police-state.jpg");
+		$("#thumb1") .attr("href", "https://zrsyndicate.wordpress.com/2016/07/08/priests-of-violence/");
+		
+
+		
+	});
